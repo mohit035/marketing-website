adc93d36c130025d11c9ea3e1f45ef3c0238b967
mohit <mohitkhadayat06@gmail.com>
2022-07-15 12:10:56 +0545

    creating selecting and appending the cokkieelement with dom api

--- script.js
+++ script.js
@@ -18,8 +18,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,21 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// creating selecting and inserting element
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  "We use cookied for immporved functionality and analytics <button class= 'btn btn--close-cookie'>Got it</button>";
+
+// header.prepend(message); this will be the first child
+header.append(message);
+
+// header.prepend(message.cloneNode(true));
+
+// header.before(message)
+// header.after(message)this are the siblings
